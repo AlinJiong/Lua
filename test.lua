419ffe30a089e58389317c973e20daaa82bf8e7c
Alinjiong <alinjiong@qq.com>
2021-07-12 18:03:55 +0800

    7.12

--- test.lua
+++ test.lua
@@ -1,19 +1,3 @@
-local db = {}
-
-local command = {}
-
-function command.GET(key)
-    return db[key]
-end
-
-function command.SET(key, value)
-    local last = db[key]
-    db[key] = value
-    return last
-end
-
-command.SET(1, 2)
-print(command.GET(1))
-print(command.SET(1, 3))
-
-print(command.GET(1))
+A = 2
+B = 2
+assert(A == B, "failed")
--- 协程.lua
+++ 协程.lua
@@ -1,27 +1,89 @@
-co =
+-- 建立一个协程
+-- coroutine.create(f)
+
+local co =
     coroutine.create(
     function()
-        for i = 1, 3 do
-            print("Before", i)
-            coroutine.yield()
-            print("After", i)
-        end
+        print("in coroutine")
+        return "coroutine return"
+    end
+)
+print(co)
+print(coroutine.resume(co))
+
+-- Output:
+-- thread: 0039B808
+-- in coroutine
+-- true    coroutine return
+
+-- 开始或启动协程
+-- coroutine.resume(co,[,val1,...])
+-- 正确的话，返回值为true和函数返回值，错误的话，返回false和错误信息
+-- 保护模式下运行，即不会中断整个流程。发生错误后，返回false+错误信息，协程状态变成dead
+local co =
+    coroutine.create(
+    function(input)
+        print("input : " .. input)
+        local param1, param2 = coroutine.yield("yield") --yield的返回值给resume
+        print("param1 is : " .. param1)
+        print("param2 is : " .. param2)
+        -- return 也会将结果返回给 resume
+        return "return"
     end
 )
 
-coroutine.resume(co)
-coroutine.resume(co)
-coroutine.resume(co)
-       
-print("...wrap...")
-function wrap(param)
-    print("Before", param)
-    obtain = coroutine.yield()
-    print("After", obtain)
-    return 3
-end
+--第一次执行,将参数传给input,yield的返回值给resume
+print(coroutine.resume(co, "function input"))
+print("this is second chunk")
+--第二次执行,将参数作为yield的返回值,传给param1 param2
+print(coroutine.resume(co, "param1", "param2"))
 
-resumer = coroutine.wrap(wrap)
-print(resumer(1))
-print("......")
-print(resumer(2))
+-- Output:
+-- input : function input
+-- true    yield
+-- this is second chunk
+-- param1 is : param1
+-- param2 is : param2
+-- true    return
+
+-- 挂起一个协程
+-- coroutine.yield(...)
+-- 传递给 yield 的参数都会转为 resume 的额外返回值
+local co =
+    coroutine.create(
+    function()
+        coroutine.yield("yield1", "yield2")
+    end
+)
+
+print(coroutine.resume(co))
+-- Output:
+-- true    yield1  yield2
+
+-- wrap
+-- 不显示错误信息，true/false，类似于creat用法
+local wrap =
+    coroutine.wrap(
+    function(input)
+        print("input : " .. input)
+        local param1, param2 = coroutine.yield("yield")
+        print("param1 is : " .. param1)
+        print("param2 is : " .. param2)
+        -- return 也会将结果返回给 resume
+        return "return"
+    end
+)
+
+--第一次执行,将参数传给input
+print(wrap("function input"))
+print("this is main chunk")
+--第二次执行,将参数作为yield的返回值,传给param1 param2
+print(wrap("param1", "param2"))
+
+-- Output:
+-- input : function input
+-- yield
+-- this is main chunk
+-- param1 is : param1
+-- param2 is : param2
+-- return
